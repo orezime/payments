a316ff5525a975638a937889fc9c13743b136b3d
jesseokeya <jesseokeya@gmail.com>
2019-11-30 00:17:44 -0500

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,6 +5,7 @@ import * as axios from 'axios'
  * Paystack test
  */
 const secretKey = 'sk_test_a30790508551ae7a406856bd6f2fa49789255454'
+
 // @ts-ignore
 axios.defaults.headers.common['Authorization'] = `Bearer ${secretKey}`
 const options = { secretKey, axios: axios, host: 'https://api.paystack.co' }
